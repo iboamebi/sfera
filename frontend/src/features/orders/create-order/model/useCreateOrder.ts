c415b045fd230222fcfec5487301690401489a73
IboAmebi <iboamebi@gmail.com>
2026-08-12 21:28:47 +0300

    refactor: support create order mutation options

--- frontend/src/features/orders/create-order/model/useCreateOrder.ts
+++ frontend/src/features/orders/create-order/model/useCreateOrder.ts
@@ -1,10 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, type UseMutationOptions } from "@tanstack/react-query";
 
 import { createOrder } from "../api/createOrder";
+import type { OrderRead } from "../../model/types";
 import type { CreateOrderForm } from "./types";
 
-export function useCreateOrder() {
+export function useCreateOrder(
+  options?: UseMutationOptions<OrderRead, Error, CreateOrderForm>,
+) {
   return useMutation({
     mutationFn: (data: CreateOrderForm) => createOrder(data),
+    ...options,
   });
 }
