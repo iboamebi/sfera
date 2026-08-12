7a63d6a5ff89400fe678de567750ff97187a5602
IboAmebi <iboamebi@gmail.com>
2026-08-12 22:29:55 +0300

    fix: align router import

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
 
 import { CreateOrderPage } from "../pages/orders/CreateOrderPage";
 import { OrderPage } from "../pages/orders/OrderPage";
