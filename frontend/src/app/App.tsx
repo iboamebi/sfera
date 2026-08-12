7d1a41b8f0584a9489d2824fdc6d4da9c60802c3
IboAmebi <iboamebi@gmail.com>
2026-08-12 22:29:32 +0300

    fix: align app router import

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router";
 
 import { QueryProvider } from "./providers/QueryProvider";
 import { router } from "./router";
