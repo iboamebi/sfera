ac1676df6e8e7b07da8353879fbae9e54a6cc364
IboAmebi <iboamebi@gmail.com>
2026-08-12 21:28:08 +0300

    feat: navigate to created order

--- frontend/src/pages/orders/CreateOrderPage.tsx
+++ frontend/src/pages/orders/CreateOrderPage.tsx
@@ -1,8 +1,15 @@
-import { CreateOrderForm } from "../../features/orders/create-order/ui/CreateOrderForm";
+import { useNavigate } from "react-router-dom";
+
 import { useCreateOrder } from "../../features/orders/create-order/model/useCreateOrder";
+import { CreateOrderForm } from "../../features/orders/create-order/ui/CreateOrderForm";
 
 export function CreateOrderPage() {
-  const { mutate } = useCreateOrder();
+  const navigate = useNavigate();
+  const { mutate } = useCreateOrder({
+    onSuccess: (order) => {
+      navigate(`/orders/${order.id}`);
+    },
+  });
 
   return <CreateOrderForm onSubmit={mutate} />;
 }
