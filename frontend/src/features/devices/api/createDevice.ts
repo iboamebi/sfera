b0ae9574ae93d06aae72bdb2cc159911e666cf9c
IboAmebi <iboamebi@gmail.com>
2026-08-21 23:09:22 +0300

    feat: add device creation API

---
+++ frontend/src/features/devices/api/createDevice.ts
@@ -0,0 +1,20 @@
+import { http } from "../../../shared/api/http";
+import { mapDevice } from "./deviceMapper";
+import type { DeviceRead } from "../model/types";
+import type { DeviceApiDto } from "./types";
+
+type CreateDeviceInput = {
+  instrumentTypeId: string;
+  serialNumber: string;
+};
+
+export async function createDevice(
+  input: CreateDeviceInput,
+): Promise<DeviceRead> {
+  const response = await http.post<DeviceApiDto>("/devices/", {
+    instrument_type_id: input.instrumentTypeId,
+    serial_number: input.serialNumber,
+  });
+
+  return mapDevice(response.data);
+}
