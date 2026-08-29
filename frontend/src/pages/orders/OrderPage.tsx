9ab4f545af516c23d0ca613a7ad7b81029ff345f
IboAmebi <iboamebi@gmail.com>
2026-08-30 01:59:24 +0300

    feat: expose mass order intake in order page

--- frontend/src/pages/orders/OrderPage.tsx
+++ frontend/src/pages/orders/OrderPage.tsx
@@ -1,6 +1,7 @@
 import { Stack, Typography } from "@mui/material";
 import { useParams } from "react-router";
 
+import { AddOrderItemsForm } from "../../features/orders/add-order-items/ui/AddOrderItemsForm";
 import { UpdateOrder } from "../../features/orders/update-order/ui/UpdateOrder";
 import { useOrder } from "../../features/orders/model/useOrder";
 import { OrderActions } from "../../features/orders/ui/OrderActions";
@@ -27,6 +28,7 @@ export function OrderPage() {
     <Stack spacing={2}>
       <OrderDetails order={data} />
       <OrderActions order={data} />
+      <AddOrderItemsForm orderId={data.id} />
       <UpdateOrder order={data} />
     </Stack>
   );
