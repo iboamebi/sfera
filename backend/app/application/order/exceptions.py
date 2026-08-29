73bd53bd3ccd83b6d2044467a5c1aafb61f6b336
IboAmebi <iboamebi@gmail.com>
2026-08-29 19:49:40 +0300

    feat: define order instrument conflict contract

--- backend/app/application/order/exceptions.py
+++ backend/app/application/order/exceptions.py
@@ -5,3 +5,7 @@ Application exceptions for Order.
 
 class OrderNotFoundApplicationError(Exception):
     """Order not found application error."""
+
+
+class InstrumentAlreadyInActiveOrderApplicationError(Exception):
+    """Instrument already belongs to another active order."""
