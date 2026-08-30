11cd1a8ff7ce21e4d1d1f7b82a7f7d33e66aa084
IboAmebi <iboamebi@gmail.com>
2026-08-30 19:07:14 +0300

    feat: add device card lookup

---
+++ frontend/src/features/devices/api/getDevice.ts
@@ -0,0 +1,9 @@
+import { http } from "../../../shared/api/http";
+import { mapDevice } from "./deviceMapper";
+import type { DeviceRead } from "../model/types";
+import type { DeviceApiDto } from "./types";
+
+export async function getDevice(deviceId: string): Promise<DeviceRead> {
+  const response = await http.get<DeviceApiDto>(`/devices/${deviceId}`);
+  return mapDevice(response.data);
+}
