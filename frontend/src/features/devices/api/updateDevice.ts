2548640ff8abb1c70cc5dee8405217f8c2d51b87
IboAmebi <iboamebi@gmail.com>
2026-08-31 20:57:18 +0300

    feat: add device card update API

---
+++ frontend/src/features/devices/api/updateDevice.ts
@@ -0,0 +1,21 @@
+import { http } from "../../../shared/api/http";
+import { mapDevice } from "./deviceMapper";
+import type { UpdateDeviceInput, DeviceRead } from "../model/types";
+import type { DeviceApiDto } from "./types";
+
+export async function updateDevice(
+  deviceId: string,
+  data: UpdateDeviceInput,
+): Promise<DeviceRead> {
+  const response = await http.put<DeviceApiDto>(`/devices/${deviceId}`, {
+    serial_number: data.serialNumber,
+    registry_number: data.registryNumber,
+    modification: data.modification,
+    factory_number: data.factoryNumber,
+    manufacture_year: data.manufactureYear,
+    inventory_number: data.inventoryNumber,
+    comment: data.comment,
+  });
+
+  return mapDevice(response.data);
+}
