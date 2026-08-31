f190c346bb2ec3f5f86672f0a9ae30ab4da3cde7
IboAmebi <iboamebi@gmail.com>
2026-08-31 20:56:50 +0300

    feat: extend device API DTO

--- frontend/src/features/devices/api/types.ts
+++ frontend/src/features/devices/api/types.ts
@@ -2,5 +2,11 @@ export interface DeviceApiDto {
   id: string;
   instrument_type_id: string;
   serial_number: string;
+  registry_number: string | null;
+  modification: string | null;
+  factory_number: string | null;
+  manufacture_year: number | null;
+  inventory_number: string | null;
+  comment: string | null;
   status: "AVAILABLE" | "IN_WORK" | "WAITING" | "COMPLETED" | "BLOCKED";
 }
