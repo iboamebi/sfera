5ca2be03a5cd271773a8b78b8c3ccc726afea6a7
IboAmebi <iboamebi@gmail.com>
2026-09-04 16:13:18 +0300

    feat: add verification creation exceptions

--- backend/app/application/verification/exceptions.py
+++ backend/app/application/verification/exceptions.py
@@ -1,2 +1,10 @@
 class VerificationNotFoundApplicationError(Exception):
     """Verification not found."""
+
+
+class VerificationOrderItemNotFoundApplicationError(Exception):
+    """Verification order item not found."""
+
+
+class VerificationInstrumentRequiredApplicationError(Exception):
+    """Verification requires a concrete instrument."""
